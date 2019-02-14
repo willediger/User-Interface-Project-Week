84b8701b1594195987c1bfbc6bf751b79e1f0095
will Ediger <will.ediger@gmail.com>
2019-02-13 19:08:52 -0600

    added dev utils, "scrollto" & "gety"

---
+++ js/devUtils.js
@@ -0,0 +1,4 @@
+//used in developing to scroll the design file and rendered page to the same pixel locations for easy comparisons down the page/design file
+
+const scrollto = y => {window.scrollTo(0, y)};
+const gety = () => window.pageYOffset;
